55164b8af9a8df7ad2defcd6b25600797c8717c2
rlefrancoise <renaud.lefrancoise@gmail.com>
2018-12-13 18:42:53 +0100

    Ship thrusters

--- Assets/Scripts/Components/ThrusterComponent.cs
+++ Assets/Scripts/Components/ThrusterComponent.cs
@@ -5,9 +5,13 @@ namespace Components
     [RequireComponent(typeof(ParticleSystem))]
     public class ThrusterComponent : MonoBehaviour
     {
+        public bool IsInitialized;
+
         public float OriginalStartSize;
         public float OriginalLifeTime;
         public Color OriginalStartColor;
+
+        public Color MinColour;
         public float Power;
     }
 }
--- Assets/Scripts/Systems/ThrusterPowerSystem.cs
+++ Assets/Scripts/Systems/ThrusterPowerSystem.cs
@@ -17,8 +17,18 @@ namespace Systems
             {
                 var thruster = entity.Thruster;
                 ParticleSystem.MainModule mainmodule = thruster.GetComponent<ParticleSystem>().main;
+
+                if (!thruster.IsInitialized)
+                {
+                    thruster.OriginalLifeTime = mainmodule.startLifetime.constant;
+                    thruster.OriginalStartSize = mainmodule.startSize.constant;
+                    thruster.OriginalStartColor = mainmodule.startColor.color;
+                    thruster.IsInitialized = true;
+                }
+
                 mainmodule.startLifetime = Mathf.Lerp(0.0f, thruster.OriginalLifeTime, thruster.Power);
                 mainmodule.startSize = Mathf.Lerp(thruster.OriginalStartSize * 0.3f, thruster.OriginalStartSize, thruster.Power);
+                mainmodule.startColor = Color.Lerp(thruster.MinColour, thruster.OriginalStartColor, thruster.Power);
             }
         }
     }
